82a77815c26acf2c4efd7526be57c95752f902d8
Sunila Gollapudi <sunila.gollapudi@broadridge.com>
2015-12-21 01:06:05 +0530

    Chapter 7 R code for Apriori based Association rules
    Chapter 7 R code for Apriori based Association rules

---
+++ r/chapter7/aprioriexample/association-rules.R
@@ -0,0 +1,40 @@
+# Practical Machine Learning 
+# Association Rules
+# Chapter 7
+
+library(arules)
+
+g <- read.transactions("groceries.csv", sep = ",")
+
+summary(g)
+
+inspect(g[1:5])
+
+itemFrequency(g[, 1:3])
+itemFrequencyPlot(g, support = 0.1)
+itemFrequencyPlot(g, topN = 20)
+
+image(g[1:5])
+image(sample(g, 100))
+
+apriori(g)
+
+grules <- apriori(g, parameter = list(support = 0.006, confidence = 0.25, minlen = 2))
+
+grules
+summary(grules)
+
+inspect(grules[1:3])
+inspect(sort(grules, by = "lift")[1:5])
+
+berryrules <- subset(grules, items %in% "berries")
+inspect(berryrules)
+
+write(grules, file = "groceryrules.csv", sep = ",", quote = TRUE, row.names = FALSE)
+
+grules_df <- as(grules, "data.frame")
+
+str(grules_df)
+
+
+
